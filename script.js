51e0ab07cd64f390d2e120299ce5125a85503f18
Your Name <you@example.com>
2022-04-11 19:46:00 -0400

    ,

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// external js: flickity.pkgd.js
+let book1 = document.querySelector('#book1');
+
+
+book1.addEventListener('click', () => {
+ 
+  let imgSrc = book1.getAttribute('src');
+
+  if (imgSrc === 'Images/book1.jpg') {
+    book1.setAttribute('src', 'Images/book1D.png');
+  } else {
+    book1.setAttribute('src', 'Images/book1.jpg');
+  }
+});
